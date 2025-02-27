ab2ad51132d14c5e869dbfc9be3daab657703ba3
Ogane-K <syokuensan@gmail.com>
2025-02-27 20:37:32 +0900

    ファイルの登録

---
+++ kadai_007/calc.js
@@ -0,0 +1,12 @@
+// 乱数を
+let num =  150
+
+if(num % 3 == 0 && num % 5 !=0 ){
+    console.log('3の倍数です');
+}else if(num % 5 == 0 && num % 3 != 0){
+    console.log('5の倍数です');
+}else if(num % 5 == 0 && num % 3 == 0){
+    console.log('5と3の倍数です');
+}else{
+    console.log(num);
+}
