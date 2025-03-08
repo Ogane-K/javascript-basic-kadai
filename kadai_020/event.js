a40f6918d9d0b5caa52de73772c6e7c0a81a66af
Ogane-K <syokuensan@gmail.com>
2025-03-09 00:12:30 +0900

    課題020ファイル追加

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('btn');
+
+
+button.addEventListener('click',() =>{
+    const h2Text = document.getElementById('text');
+
+    h2Text.textContent= 'ボタンをクリックしました。';
+
+})
